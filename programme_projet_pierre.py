2a636fc9090a092ddf1de0f281943ecfb9b1365c
Pierre <pierre.divoux@eivp-paris.fr>
2020-11-11 13:53:44 +0100

    modification python 11/11/2020 13:54

--- programme_projet_pierre.py
+++ programme_projet_pierre.py
@@ -4,8 +4,12 @@ import datetime
 default_sd = datetime.datetime(2018, 1, 1, 0, 0, 0)
 default_ed = datetime.datetime(2021, 1, 1, 0, 0, 0)
 
-def programme_1(x, start_date = default_sd, end_date = default_ed) :
+def programme_1(x, start_date = default_sd.isoformat(' '), end_date = default_ed.isoformat(' ')) :
     liste_abs = []
     liste_ord = []
+    matrice = pd.read_csv('EIVP_KM.csv', sep = ';', header = None)
 
+
+
+datetime.strptime('2019-08-25 11:45:54+02:00', '%y-%m-%d %H-%M-%S')
 
