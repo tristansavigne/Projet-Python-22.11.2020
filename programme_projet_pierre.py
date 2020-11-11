62ebc139633c8c189de496faa9ab87d950a9a4c6
Pierre <pierre.divoux@eivp-paris.fr>
2020-11-11 12:41:09 +0100

    enregistrement fichier python Pierre 11/11/2020 e Please enter the commit message for your changes. Lines starting

---
+++ programme_projet_pierre.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime
+default_sd = datetime.datetime(2018, 1, 1, 0, 0, 0)
+default_ed = datetime.datetime(2021, 1, 1, 0, 0, 0)
+
+def programme_1(x, start_date = default_sd, end_date = default_ed) :
+    liste_abs = []
+    liste_ord = []
+
+
