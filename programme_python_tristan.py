8414db61cb22039a4efc92fc0e8818ca89e987ae
Tristan <tristansavigne@gmail.com>
2020-11-20 16:53:41 +0100

    enregistrement 20/11/2020 (2) e Please enter the commit message for your changes. Lines starting

--- programme_python_tristan.py
+++ programme_python_tristan.py
@@ -132,4 +132,11 @@ def programme_4( x, y, start_date = default_sd, end_date = default_ed) :
     plt.plot(liste_abs, liste_ord)
     plt.show()
 
+
+    plt.plot(liste_abs, liste_ord1, label = x)
+    plt.title('r = ' + str(r))
+    plt.plot(liste_abs, liste_ord2, label = y)
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left', ncol=2, mode="expand", borderaxespad=0.)
+    plt.show()
+
 #corriger 4
